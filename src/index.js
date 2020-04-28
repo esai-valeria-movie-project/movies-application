bb7d831ca5a0b5e9db40a2c19bbdde0ea8ce6d8c
Valeria Meza <valeriameza173@gmail.com>
2020-04-28 12:26:05 -0500

    completed the first part of the project.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-/**
- * es6 modules and imports
- */
-import sayHello from './hello';
-sayHello('World');
+const $ = require('jquery');
 
-/**
- * require style imports
- */
-const {getMovies} = require('./api.js');
+$(document).ready(function () {
+    console.log("jquery");
+    const {getMovies} = require('./api.js');
+    const loading = $("#loading");
+    const done = $(".empty");
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+    getMovies().then((movies) => {
+        console.log('Here are all the movies:');
+        movies.forEach(({title, rating, id}) => {
+            console.log(`id#${id} - ${title} - rating: ${rating}`);
+            let result = "";
+            result += "<h5>Movie ID: " + id + "</h5>";
+            result += "<h3>Title: " + title + "</h3>";
+            result += "<h4>Rating: " + rating + "</h4>";
+            console.log(result);
+            loading.append(result);
+            done.empty();
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
+    });
 });
