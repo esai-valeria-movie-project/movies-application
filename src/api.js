cab37758f4e3217c48fb22a78ff05645e4525230
Esai Flores <flores.esaijonathan@gmail.com>
2020-04-28 15:01:36 -0500

    took away catch because breaking code

--- src/api.js
+++ src/api.js
@@ -10,16 +10,12 @@ module.exports = {
     const options = {
       method: 'POST',
       headers: {
-        'Content-Type' : 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(post),
     };
     fetch(url, options)
         .then(response => response.json())
-        .catch((error) => {
-      alert('wrong way')
-          console.log(error);
-        });
-    console.log(addMovies)
+        .catch()
   }
 };
