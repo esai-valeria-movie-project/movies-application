0b5a47c2a78283d2aa80746d2ed9a3279b8df091
Esai Flores <flores.esaijonathan@gmail.com>
2020-04-30 10:13:19 -0500

    added genre functionality

--- src/index.js
+++ src/index.js
@@ -20,10 +20,11 @@ $(document).ready(function () {
     const editMovie = $('#edit');
     const editID = $('#id');
     const editMovieName = $('#edit-movie');
+    const editGenre = $('#edit-genre');
     const editRating = $('#edit-rating');
 
     editMovie.click(function () {
-        editMovies(editMovieName.val(), editRating.val(), editID.val());
+        editMovies(editMovieName.val(), editRating.val(),  editGenre.val(), editID.val());
     });
 
 
@@ -37,13 +38,14 @@ $(document).ready(function () {
 
     getMovies().then((movies) => {
         console.log('Here are all the movies:');
-        movies.forEach(({title, rating, id}) => {
-            console.log(`id#${id} - ${title} - rating: ${rating}`);
+        movies.forEach(({title, rating, id, genre}) => {
+            console.log(`id#${id} - ${title} - rating: ${rating} - genre: ${genre}`);
 
             let result = "" +
             "<div class=\"carousel-item\"><img src=\"https://cdn4.iconfinder.com/data/icons/small-n-flat/24/movie-alt2-512.png\" class=\"d-block w-100\" alt=\"...\">"+
                 "<div class='carousel-caption d-none d-md-block'>" +
                 "<h5>Title: " + title + "</h5>" +
+                "<h4>Genre: " + genre + "</h4>"+
                 "<p> Rating: " + rating + "</p>" +
                 "<p>Movie id: " + id + "</p>"+
                 "</div>" +
